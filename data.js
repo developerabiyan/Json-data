b7117d1d57c78ae554ad6637cd7826cf3cad9539
iftikhar <test1@abc.com>
2024-01-26 09:29:31 +0500

    first message

---
+++ data.js
@@ -0,0 +1,65 @@
+var data=[
+    {
+        id : 101,
+        Name : "Imran Khan",
+        Contact : 3303438347,
+        employee : 10012,
+        profile : "img/1.jpg"
+    },
+    {
+        id : 102,
+        Name : "Israr Khan",
+        Contact : 3326439747,
+        employee : 10023,
+        profile : "img/2.jpg"
+    },
+    {
+        id : 103,
+        Name : "Ahsan Khan",
+        Contact : 3303456847,
+        employee : 10278,
+        profile : "img/3.jpg"
+    },
+    {
+        id : 104,
+        Name : "Abiyan Qureshi",
+        Contact : 3356898347,
+        employee : 10679,
+        profile : "img/4.jpg"
+    },
+    {
+        id : 105,
+        Name : "jack denils",
+        Contact : 3303326747,
+        employee : 16697,
+        profile : "img/5.jpg"
+    },
+    {
+        id : 106,
+        Name : "jack oggy",
+        Contact : 3303997747,
+        employee : 199897,
+        profile : "img/6.jpg"
+    },
+    {
+        id : 107,
+        Name : "jack harly",
+        Contact : 3303886747,
+        employee : 15597,
+        profile : "img/7.jpg"
+    },
+    {
+        id : 108,
+        Name : "jack usiir",
+        Contact : 3303889747,
+        employee : 188897,
+        profile : "img/8.jpg"
+    },
+    {
+        id : 109,
+        Name : "abiyan",
+        Contact : 3303666747,
+        employee : 10997,
+        profile : "img/9.jpg"
+    },
+]
